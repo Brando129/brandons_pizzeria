5edb5a6759500889633a577b429d03c2102c6a7d
Brandon Colangelo <brandoncancode@gmail.com>
2023-09-06 13:22:13 -0400

    Initial Commit.

---
+++ flask_app/controllers/controllers_users.py
@@ -0,0 +1,13 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash
+from flask_app.models import models_user, models_pizza
+# Bcrypt import
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app) # We are creating an object called bcrypt,
+# which is made by invoking the function Bcrypt with our app as an argument.
+
+# Get Routes
+# Route for rendering the Registration page.
+@app.route('/')
+def registration():
+    return render_template('registration.html')
